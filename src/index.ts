fbdac68a508398f5cfa5a3d2583920aa64212206
shreyas <shreyas.mohanty03@gmail.com>
2024-05-27 11:54:42 +0530

    refactor: index file refactored with better security

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
+require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
+const corsOptions = {
+  origin: "*",
+  methods: "GET, POST, PATCH, PUT, HEAD, DELETE",
+  credential: true,
+};
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+const userAuthRoutes = require("./routes/auth.ts");
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+app.use("/api/auth/", userAuthRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
