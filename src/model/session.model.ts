cc87b16fe0032dbf828dda71ee8d5296d6d76b9f
shreyas <shreyas.mohanty03@gmail.com>
2024-05-27 11:55:24 +0530

    feat(model): session model created

---
+++ src/model/session.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const sessionSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  sessionId: { type: String, required: true },
+  expiresAt: {
+    type: Date,
+    required: true,
+    default: () => {
+      const date = new Date();
+      date.setDate(date.getDate() + 1);
+      return date;
+    },
+  },
+});
+
+const Session = models.Session || model("Session", sessionSchema);
+export default Session;
