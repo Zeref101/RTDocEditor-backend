285e8d007aa63d32b7d32c31f01f8405a90a7c8a
shreyas <shreyas.mohanty03@gmail.com>
2024-05-28 17:36:42 +0530

    refactor: Update User model password field to be optional

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -3,8 +3,9 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
   avatar: { type: String, required: true },
+  googleAuth: { type: Boolean, default: false },
 });
 
 const User = models.User || model("User", userSchema);
