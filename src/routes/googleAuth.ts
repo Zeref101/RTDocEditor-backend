dd2a5b7650b97166a1ab8549f435d14d78cfe0d8
shreyas <shreyas.mohanty03@gmail.com>
2024-05-28 16:18:54 +0530

    feat(auth): google auth using passport created

---
+++ src/routes/googleAuth.ts
@@ -0,0 +1,50 @@
+import { Router, Response, Request } from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+const router = Router();
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
+
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+  throw new Error(
+    "Google client ID and secret must be set in environment variables"
+  );
+}
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj: any, done) => {
+  done(null, obj);
+});
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:3000/signIn",
+  }),
+  function (req, res) {
+    console.log(req.user);
+    res.redirect("http://localhost:3000/");
+  }
+);
+module.exports = router;
