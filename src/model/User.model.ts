32afb99eeee8c235ef7bf0493d5225c3fcb9b810
shreyas <shreyas.mohanty03@gmail.com>
2024-05-30 13:55:08 +0530

    refactor: Make avatar field optional in User model

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -4,7 +4,7 @@ const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String },
-  avatar: { type: String, required: true },
+  avatar: { type: String },
   googleAuth: { type: Boolean, default: false },
 });
 
