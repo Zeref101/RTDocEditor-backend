9c3e45458a8f284d8a594028e316df0b35a33a10
shreyas <shreyas.mohanty03@gmail.com>
2024-06-05 18:47:19 +0530

    refactor: Update document routes to improve functionality

---
+++ src/routes/document.ts
@@ -0,0 +1,87 @@
+import express, { Request, Response } from "express";
+import Document from "../model/Document.model";
+import { connectToDB } from "../db";
+
+const router = express.Router();
+
+// GET /documents/:id
+router.get("/documents/:id", async (req: Request, res: Response) => {
+  const document = await Document.findById(req.params.id);
+  if (!document) return res.status(404).send("Document not found");
+  res.send(document);
+});
+
+// POST /documents
+router.post("/documents", async (req: Request, res: Response) => {
+  try {
+    connectToDB();
+    const document = new Document({
+      content: req.body.content,
+      title: req.body.title,
+      owner: req.body.owner,
+      lastModified: new Date(),
+    });
+    await document.save();
+    res.status(201).send(document);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send({ message: "An error occurred while creating the document." });
+  }
+});
+
+// PUT /documents/:id
+router.put("/documents/:id", async (req: Request, res: Response) => {
+  console.log(req.params.id);
+  connectToDB();
+  const document = await Document.findByIdAndUpdate(
+    req.params.id,
+    {
+      content: req.body.content,
+      lastModified: new Date(),
+    },
+    { new: true }
+  );
+  if (!document) return res.status(404).send("Document not found");
+  res.send(document);
+});
+
+// DELETE /documents/:id
+router.delete("/documents/:id", async (req: Request, res: Response) => {
+  const document = await Document.findByIdAndDelete(req.params.id);
+  if (!document) return res.status(404).send("Document not found");
+  res.send(document);
+});
+
+// UPDATE TITLE
+router.put("/document/:docId", async (req, res) => {
+  const { docId } = req.params;
+  const { title } = req.body;
+
+  try {
+    connectToDB();
+    const document = await Document.findByIdAndUpdate(
+      docId,
+      { title },
+      { new: true }
+    );
+    if (!document) {
+      return res.status(404).json({ message: "Document not found" });
+    }
+    res.json(document);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating document", error });
+  }
+});
+router.get("/documents", async (req, res) => {
+  try {
+    connectToDB();
+    const documents = await Document.find({}, "_id title");
+    res.json(documents.map((doc) => ({ id: doc._id, title: doc.title })));
+  } catch (error) {
+    res.status(500).json({ message: "Error getting documents", error });
+  }
+});
+
+module.exports = router;
