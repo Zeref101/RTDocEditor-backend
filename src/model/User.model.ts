7f39d2b1150fabbe81d804266636d5a0f245f352
shreyas <shreyas.mohanty03@gmail.com>
2024-06-06 01:09:52 +0530

    refactor: Add savedDocuments field to User model
    This commit adds a new field `savedDocuments` to the `User` model in `User.model.ts`. The `savedDocuments` field is an array of `ObjectId` references to the `Document` model. This change enhances the functionality of the model by allowing users to save and reference documents.

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -3,6 +3,7 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
+  savedDocuments: { type: [Schema.ObjectId], ref: "Document" },
   password: { type: String },
   avatar: { type: String },
   googleAuth: { type: Boolean, default: false },
