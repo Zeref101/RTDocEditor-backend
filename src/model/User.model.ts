8c9c6608a08b2f89b02170c9d17cdb9e3a2b789a
shreyas <shreyas.mohanty03@gmail.com>
2024-06-07 14:11:38 +0530

    feat: Add personal field to User model
    The code changes in this commit add a new field `personal` to the `User` model in `User.model.ts`. This field is of type `Schema.ObjectId` and allows users to store a personal reference. This change enhances the functionality of the model by providing a way for users to associate a personal object with their account.

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -3,6 +3,7 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
+  personal: { type: Schema.ObjectId },
   savedDocuments: { type: [Schema.ObjectId], ref: "Document" },
   password: { type: String },
   avatar: { type: String },
